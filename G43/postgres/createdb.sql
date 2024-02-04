9be628c35872a6b395e3f1d66cb49c695ca01697
雾岛 zhang <145342600+zhang-mickey@users.noreply.github.com>
2024-02-04 12:17:42 +0100

    Create createdb.sql

---
+++ G43/postgres/createdb.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE my_db;
+CREATE TABLE groceries (id SERIAL PRIMARY KEY, name TEXT);
+
+INSERT INTO groceries (name) VALUES ('apples');
+INSERT INTO groceries (name) VALUES ('bananas');
+INSERT INTO groceries (name) VALUES ('clemintines');
+INSERT INTO groceries (name) VALUES ('cheese');
+INSERT INTO groceries (name) VALUES ('cilantro');
+INSERT INTO groceries (name) VALUES ('pineapple');
+INSERT INTO groceries (name) VALUES ('strawberry');
+INSERT INTO groceries (name) VALUES ('watermelon');
+INSERT INTO groceries (name) VALUES ('dragonfruit');
